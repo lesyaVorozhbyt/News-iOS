a546ba538cb6c246d8eba9ced4b5a98dcf6e7398
Yurii Baidetskyi <baidetskyi.yurii@gmail.com>
2022-10-06 20:25:27 +0300

    test

---
+++ closure 2.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+
+func printArray(isABS: Bool, arrayCloser: (_ array: [Int], _ isABS: Bool) -> [Int]) {
+    var array = [15, 3, 7, 9, 20]
+    array = arrayCloser(array, isABS)
+    print(array)
+}
+
+func sortingArray(items: [Int], isABS: Bool) -> [Int] {
+    print("sortingArray function")
+    return items
+}
+
+printArray(isABS: true, arrayCloser: sortingArray)
+
+printArray(isABS: false) { array, isABS  in
+    return [1, 2, 3, 4, 5]
+}
+
+printArray(isABS: true) { array, isABS in [1, 2, 3, 4, 5] }
+
+// ==
+printArray(isABS: false) { array, isUp  in
+    array.sorted()
+}
+// ==
+printArray (isABS: true) {
+    $0.sorted()
+    print("\($1)")
+    return $0
+
+}
