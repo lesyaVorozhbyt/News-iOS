2b826307ae21203ecc505fac7485398528b9a7fc
Yurii Baidetskyi <baidetskyi.yurii@gmail.com>
2022-10-28 20:07:16 +0300

    newProjectCreated

---
+++ NewsIOS/NewsIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsIOS
+//
+//  Created by Baidetskyi Jurii on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
