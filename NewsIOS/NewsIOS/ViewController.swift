52e935c31efdc254bc6821aeeeae64de3403be31
Yurii Baidetskyi <baidetskyi.yurii@gmail.com>
2022-10-28 20:28:19 +0300

    Background color is changed in VC [NW-1]

--- NewsIOS/NewsIOS/ViewController.swift
+++ NewsIOS/NewsIOS/ViewController.swift
@@ -4,16 +4,13 @@
 //
 //  Created by Baidetskyi Jurii on 28.10.2022.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
-
-
 }
 
